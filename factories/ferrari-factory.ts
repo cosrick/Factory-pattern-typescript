d089864b0827ffb0cf30be434891c3d96de49f42
rick830620@gmail.com <rick830620@gmail.com>
2021-11-21 15:28:00 +0800

    remove redundant files

--- factories/ferrari-factory.ts
+++
@@ -1,9 +0,0 @@
-import { Car, Factory } from "../interface";
-import { Ferrari } from "../products";
-
-export class FerrariFactory implements Factory {
-  
-  public produceCar(license: string): Car {
-    return new Ferrari(license);
-  }
-}
